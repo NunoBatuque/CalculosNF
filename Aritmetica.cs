a14609e0bbcd9262675676c4fdab19a78b30d40b
NunoBatuque <181661257+NunoBatuque@users.noreply.github.com>
2024-09-22 19:24:07 +0100

    Criar a Classe Conversões e mover Método

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -9,17 +9,7 @@ namespace Calculos
     //Implementa alguns calculos aritmeticos
     internal static class Aritmetica
     {
-        /// <summary>
-        /// Tipo de conversão de temperatura a executar.
-        /// </summary>
-        public enum ConversaoTemperatura
-        {
-            Nulo = 0,
-            CelsiusFahrenheit,
-            FahrenheitCelsius
-        }
-
-
+        
         /// <summary>
         /// Operação soma.
         /// </summary>
@@ -37,19 +27,7 @@ namespace Calculos
         {
             return x - y;
         }
-        public static double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
-        {
-            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
-            {
-                return (temperatura * 1.8000 + 32);
-            }
-            else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
-            {
-                return ((temperatura - 32) / 1.8000);
-            }
-
-            return -1;
-        }
+       
 
 
     }
